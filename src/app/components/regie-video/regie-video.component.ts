cfa4c8502a816fc8549bf09f85782b3b466e0055
JérémyGrmt <59418445+JeremyGrmt@users.noreply.github.com>
2024-03-11 00:06:17 +0100

    affichage d'une video
    corrigé le probleme de non lancement automatique de la video au refresh de la page

--- src/app/components/regie-video/regie-video.component.ts
+++ src/app/components/regie-video/regie-video.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-regie-video',
   templateUrl: './regie-video.component.html',
   styleUrls: ['./regie-video.component.scss']
 })
-export class RegieVideoComponent {
-
+export class RegieVideoComponent implements OnInit{
+  constructor(private elementRef: ElementRef) {}
+  videoUrl:string = ''
+  
+  ngOnInit(): void {
+      this.videoUrl = "http://localhost:7001/video_1.mp4"
+  }
 }
