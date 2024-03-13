f06c6c1bbc0fdffb8cd1690175c8e48203da99ec
JérémyGrmt <59418445+JeremyGrmt@users.noreply.github.com>
2024-03-13 22:53:36 +0100

    v1 avec une video ok
    à ameliorer :
    
    faire plusieurs div : 1 par ligne et leurs donner un id différent

--- src/app/components/regie-video/regie-video.component.ts
+++ src/app/components/regie-video/regie-video.component.ts
@@ -10,6 +10,36 @@ export class RegieVideoComponent implements OnInit{
   videoUrl:string = ''
   
   ngOnInit(): void {
-      this.videoUrl = "http://localhost:7001/video_1.mp4"
+      this.videoUrl = "http://localhost:7001/video_1.mp4";
+      const video = this.elementRef.nativeElement.querySelector('video');
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+      }
+
+      const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Element is visible in the viewport
+          console.log('video jouée');
+          video.muted = true;
+          video.play();
+          // Faire quelque chose lorsque l'élément est visible
+        } else {
+          // Element is not visible in the viewport
+          console.log('vidéo mise en pause');
+          video.pause();
+          // Faire quelque chose lorsque l'élément n'est pas visible
+        }
+      });
+    }, options);
+
+    // Récupérer l'élément cible
+    const target = this.elementRef.nativeElement.querySelector('#intersectionTarget');
+    
+
+    // Commencer à observer l'élément cible
+    observer.observe(target);
   }
 }
