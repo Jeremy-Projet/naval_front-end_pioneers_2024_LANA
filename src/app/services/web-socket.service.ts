512de48f6f83e115d59d03b1115c42056863e708
JeremyG <59418445+JeremyGrmt@users.noreply.github.com>
2024-03-18 20:23:30 +0100

    Revert "Tableau"

--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -6,10 +6,6 @@ import { Injectable } from '@angular/core';
 export class WebsocketService {
   public socket: WebSocket;
 
-
-  private socket$!: WebSocketSubject<any>;
-  public socket: WebSocket;
-
   constructor() {
     this.socket = new WebSocket('ws://localhost:7000/ws/simulation/sitac');
     this.socket.onopen = (event) => {
@@ -22,19 +18,5 @@ export class WebsocketService {
     this.socket.onclose = (event) => {
       console.log('Connexion WebSocket fermée.');
     };
-
-
-  }
-
-  connect(url: string): void {
-
-    this.socket$ = webSocket(url);
-
-    this.socket$.subscribe(
-      (message) => {
-        console.log('Message reçu:', message);
-      }
-    );
-
   }
 }
